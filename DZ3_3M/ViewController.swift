174477fb2933af44a67c98a245d4f7165b2cda0c
Nurlan Seitov <seitov@gmail.com>
2023-02-25 20:24:56 +0600

    Initial Commit

---
+++ DZ3_3M/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ3_3M
+//
+//  Created by Nurlan Seitov on 25/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
